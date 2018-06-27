a7882cbf44dc2b325006f08f3e0181ba880e7298
butter007 <butter_007@163.com>
2018-06-27 23:03:04 +0800

    封装函数修改方法有bug

--- spiderrate.py
+++ spiderrate.py
@@ -4,9 +4,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import re
 from selenium.webdriver.chrome.options import Options
+import sqlite3
+from lxml import html
 
 
-def get_content(url):
+def get_content(url, searchDate, type, page=1):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
@@ -30,10 +32,10 @@ def get_content(url):
     #    currency_selectinput_tag.select_by_value('1316')
     #    browser.find_elements_by_class_name('search_btn')[1].click()
     jsformsubmit = "document.pageform.submit()"
-    jserectDate = "$('input[type=hidden][name=erectDate]').attr('value','2018-06-13')"
-    jsnothing = "$('input[type=hidden][name=nothing]').attr('value','2018-06-13')"
-    jspage = "$('input[type=hidden][name=page]').attr('value','2')"
-    jspjname = "$('input[type=hidden][name=pjname]').attr('value','1316')"
+    jserectDate = "$('input[type=hidden][name=erectDate]').attr('value',%s)" % searchDate
+    jsnothing = "$('input[type=hidden][name=nothing]').attr('value',%s)" % searchDate
+    jspage = "$('input[type=hidden][name=page]').attr('value',%s)" % page
+    jspjname = "$('input[type=hidden][name=pjname]').attr('value',%s)" % type
     browser.execute_script(jserectDate)
     browser.execute_script(jsnothing)
     browser.execute_script(jspage)
@@ -45,14 +47,36 @@ def get_content(url):
     return browser.page_source
 
 
-
-if __name__ == '__main__':
-    url = 'http://srh.bankofchina.com/search/whpj/search.jsp'
-    content = get_content(url)
+def getPage(content):
     pat = re.compile('m_nRecordCount = \d+')
     li = pat.findall(content)
     if li:
         mcounts = li[0].split()[2]
-    print(mcounts)
     pages = int(mcounts) // 20 + 1
-    print(pages)
+    etree = html.etree
+    htmlcon = etree.HTML(content)
+    result = htmlcon.xpath('//div[@class="BOC_main publish"]/table/tr/td/text()')
+    datalist = [result[i:i + 8] for i in range(0, len(result), 8)]
+    if len(datalist[-1]) < 8:
+        datalist.pop()
+    print(datalist)
+    return pages
+
+
+if __name__ == '__main__':
+    url = 'http://srh.bankofchina.com/search/whpj/search.jsp'
+    content = get_content(url, '2018-6-13', '1316')
+    page = getPage(content)
+    con = sqlite3.connect('e:/mydatabase.db3')
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE rate (
+                                id INTERGER PRIMARY KEY,
+                                name VARCHAR(40),
+                                currencybuy FLOAT,
+                                cashbuy FLOAT,
+                                currencysell FLOAT,
+                                cashbuy FLOAT,
+                                cbankprice FLOAT,
+                                publishdate VARCHAR(20),
+                                publishtime VARCHAR(20))""")
+
